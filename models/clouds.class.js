4aa7119289e698be06589ffbddecef10291d450b
Amalia Konstantinidou <130513280+Ama-kon@users.noreply.github.com>
2024-04-27 15:57:01 +0200

    only for testing

---
+++ models/clouds.class.js
@@ -0,0 +1,12 @@
+class clouds extends movableObject {
+  y = 0;
+  x = 720;
+  height = 150;
+  width = 200;
+
+  constructor() {
+    super().loadIMG("img/3. Background/Barrier/1.png");
+
+    // this.moveToLeft();
+  }
+}
