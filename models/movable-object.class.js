2d5e860fb8dcf35c278f8a0eba2491c1e4f83c5d
Amalia Konstantinidou <130513280+Ama-kon@users.noreply.github.com>
2024-04-27 15:55:46 +0200

    load img's

---
+++ models/movable-object.class.js
@@ -0,0 +1,34 @@
+class movableObject {
+  x = 80;
+  y = 250;
+  img;
+  height = 150;
+  width = 100;
+  imageCache = {};
+  currentIMG = 0;
+  speed = 0.33;
+
+  loadIMG(path) {
+    this.img = new Image();
+    this.img.src = path;
+  }
+
+  loadImages(array) {
+    array.forEach((path) => {
+      this.img = new Image();
+      this.img.src = path;
+      this.imageCache[path] = this.img;
+    });
+  }
+
+  swimUp() {}
+
+  swimDown() {}
+  swimLeft() {
+    setInterval(() => {
+      this.x -= this.speed;
+    }, 1000 / 60);
+  }
+
+  swimRight() {}
+}
