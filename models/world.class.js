f190a1142e50cefc06bd864e2f001059baf7feed
Amalia Konstantinidou <130513280+Ama-kon@users.noreply.github.com>
2024-05-15 17:07:37 +0200

    added new background

--- models/world.class.js
+++ models/world.class.js
@@ -1,113 +1,37 @@
 class World {
   character = new mainCharacter();
-  enemies = [
-    new enemyGreenFish(),
-    new enemyRedFish(),
-    new enemyJellyfishLila(),
-    new enemyJellyfishYellow(),
-  ];
-
-  clouds = [new clouds()];
-  background = [
-    new BackgroundObjects("img/3. Background/Layers/5. Water/D2.png", -720, 0),
-    new BackgroundObjects("img/3. Background/Layers/1. Light/2.png", -720, 0),
-    new BackgroundObjects("img/3. Background/Layers/4.Fondo 2/D2.png", -720, 0),
-    new BackgroundObjects("img/3. Background/Layers/3.Fondo 1/D2.png", -720, 0),
-    new BackgroundObjects("img/3. Background/Layers/2. Floor/D2.png", -720, 0),
-
-    new BackgroundObjects("img/3. Background/Layers/5. Water/D1.png", 0, 0),
-    new BackgroundObjects("img/3. Background/Layers/1. Light/1.png", 0, 0),
-    new BackgroundObjects("img/3. Background/Layers/4.Fondo 2/D1.png", 0, 0),
-    new BackgroundObjects("img/3. Background/Layers/3.Fondo 1/D1.png", 0, 0),
-    new BackgroundObjects("img/3. Background/Layers/2. Floor/D1.png", 0, 0),
-
-    new BackgroundObjects("img/3. Background/Layers/5. Water/D2.png", 720, 0),
-    new BackgroundObjects("img/3. Background/Layers/1. Light/2.png", 720, 0),
-    new BackgroundObjects("img/3. Background/Layers/4.Fondo 2/D2.png", 720, 0),
-    new BackgroundObjects("img/3. Background/Layers/3.Fondo 1/D2.png", 720, 0),
-    new BackgroundObjects("img/3. Background/Layers/2. Floor/D2.png", 720, 0),
-
-    new BackgroundObjects(
-      "img/3. Background/Layers/5. Water/D1.png",
-      720 * 2,
-      0
-    ),
-    new BackgroundObjects(
-      "img/3. Background/Layers/1. Light/1.png",
-      720 * 2,
-      0
-    ),
-    new BackgroundObjects(
-      "img/3. Background/Layers/4.Fondo 2/D1.png",
-      720 * 2,
-      0
-    ),
-    new BackgroundObjects(
-      "img/3. Background/Layers/3.Fondo 1/D1.png",
-      720 * 2,
-      0
-    ),
-    new BackgroundObjects(
-      "img/3. Background/Layers/2. Floor/D1.png",
-      720 * 2,
-      0
-    ),
-
-    new BackgroundObjects(
-      "img/3. Background/Layers/5. Water/D2.png",
-      720 * 3,
-      0
-    ),
-    new BackgroundObjects(
-      "img/3. Background/Layers/1. Light/2.png",
-      720 * 3,
-      0
-    ),
-    new BackgroundObjects(
-      "img/3. Background/Layers/4.Fondo 2/D2.png",
-      720 * 3,
-      0
-    ),
-    new BackgroundObjects(
-      "img/3. Background/Layers/3.Fondo 1/D2.png",
-      720 * 3,
-      0
-    ),
-    new BackgroundObjects(
-      "img/3. Background/Layers/2. Floor/D2.png",
-      720 * 3,
-      0
-    ),
-  ];
+  level = level1;
 
   ctx;
   canvas;
   keyboard;
   camera_x = 0;
+  background_music = new Audio("audio/background_music.mp3");
 
   constructor(canvas, keyboard) {
     this.canvas = canvas;
     this.keyboard = keyboard;
     this.ctx = canvas.getContext("2d");
-    this.draw();
+    this.drawAll();
     this.setWorld();
+    this.checkForCollision();
+    // this.background_music.play(); // funktioniert - erst wieder öffnen wenn fertig
   }
 
   setWorld() {
     this.character.world = this;
   }
-  draw() {
+  drawAll() {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
     this.ctx.translate(this.camera_x, 0);
-    this.addObjects(this.background);
-    this.addObjects(this.enemies);
-
+    this.addObjects(this.level.background);
+    this.addObjects(this.level.enemies);
     this.addToCanvas(this.character);
-    this.addObjects(this.clouds);
+    this.addObjects(this.level.clouds);
     this.ctx.translate(-this.camera_x, 0);
     let self = this;
     requestAnimationFrame(function () {
-      self.draw();
+      self.drawAll();
     });
   }
 
@@ -117,21 +41,24 @@ class World {
 
   addToCanvas(object) {
     if (object.otherDirection) {
-      this.ctx.save();
-      this.ctx.translate(object.width, 0);
-      this.ctx.scale(-1, 1);
-      object.x = object.x * -1;
+      object.flipImage(this.ctx);
     }
-    this.ctx.drawImage(
-      object.img,
-      object.x,
-      object.y,
-      object.width,
-      object.height
-    );
+    object.draw(this.ctx);
+    object.drawRectangle(this.ctx);
+
     if (object.otherDirection) {
-      object.x = object.x * -1;
-      this.ctx.restore();
+      object.flipImageBack(this.ctx);
     }
+    // this.drawRectangle(object.x, object.y, object.width, object.height); // zum berechnen des zusammenstoßens, kann danach raus
+  }
+
+  checkForCollision() {
+    setInterval(() => {
+      this.level.enemies.forEach((enemy) => {
+        if (this.character.isColliding(enemy)) {
+          console.log(`Collision with ${enemy}!`);
+        }
+      });
+    }, 1000);
   }
 }
