17a1090b1c217b013e0817110997751f7608a06d
Amalia Konstantinidou <130513280+Ama-kon@users.noreply.github.com>
2024-05-15 17:07:59 +0200

    impl. endboss

---
+++ models/endboss.class.js
@@ -0,0 +1,43 @@
+class Endboss extends movableObject {
+  height = 400;
+  width = 400;
+  y = 0;
+  x = 400;
+  images_move = [
+    "img/2.Enemy/3 Final Enemy/1.Introduce/1.png",
+    "img/2.Enemy/3 Final Enemy/1.Introduce/2.png",
+    "img/2.Enemy/3 Final Enemy/1.Introduce/3.png",
+    "img/2.Enemy/3 Final Enemy/1.Introduce/4.png",
+    "img/2.Enemy/3 Final Enemy/1.Introduce/5.png",
+    "img/2.Enemy/3 Final Enemy/1.Introduce/6.png",
+    "img/2.Enemy/3 Final Enemy/1.Introduce/7.png",
+    "img/2.Enemy/3 Final Enemy/1.Introduce/8.png",
+    "img/2.Enemy/3 Final Enemy/1.Introduce/9.png",
+    "img/2.Enemy/3 Final Enemy/1.Introduce/10.png",
+
+    "img/2.Enemy/3 Final Enemy/2.floating/1.png",
+    "img/2.Enemy/3 Final Enemy/2.floating/2.png",
+    "img/2.Enemy/3 Final Enemy/2.floating/3.png",
+    "img/2.Enemy/3 Final Enemy/2.floating/4.png",
+    "img/2.Enemy/3 Final Enemy/2.floating/5.png",
+    "img/2.Enemy/3 Final Enemy/2.floating/6.png",
+    "img/2.Enemy/3 Final Enemy/2.floating/7.png",
+    "img/2.Enemy/3 Final Enemy/2.floating/8.png",
+    "img/2.Enemy/3 Final Enemy/2.floating/9.png",
+    "img/2.Enemy/3 Final Enemy/2.floating/10.png",
+    "img/2.Enemy/3 Final Enemy/2.floating/11.png",
+    "img/2.Enemy/3 Final Enemy/2.floating/12.png",
+    "img/2.Enemy/3 Final Enemy/2.floating/13.png",
+  ];
+  constructor() {
+    super().loadIMG("img/2.Enemy/3 Final Enemy/1.Introduce/1.png");
+
+    this.loadImages(this.images_move);
+    this.animate();
+  }
+  animate() {
+    setInterval(() => {
+      this.playAnimation(this.images_move);
+    }, 150);
+  }
+}
