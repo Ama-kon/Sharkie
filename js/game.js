5f97f9d0602d7605d2988a071e5bba46bcf6ee88
Amalia Konstantinidou <130513280+Ama-kon@users.noreply.github.com>
2024-05-23 21:06:44 +0200

    first impl. of sounds (+ on/off)

--- js/game.js
+++ js/game.js
@@ -1,9 +1,11 @@
 let canvas;
 let world;
 let keyboard = new Keyboard();
+let character;
 function init() {
   canvas = document.getElementById("canvas");
   world = new World(canvas, keyboard);
+  character = world.character;
 }
 
 window.addEventListener("keydown", (event) => {
@@ -21,6 +23,9 @@ window.addEventListener("keydown", (event) => {
   }
   if (event.keyCode == 39) {
     keyboard.right = true;
+  }
+  if (event.keyCode == 68) {
+    keyboard.d = true;
   }
 });
 
@@ -40,4 +45,37 @@ window.addEventListener("keyup", (event) => {
   if (event.keyCode == 39) {
     keyboard.right = false;
   }
+  if (event.keyCode == 68) {
+    keyboard.d = false;
+  }
 });
+
+function toggleImage() {
+  let img = document.getElementById("sound_toggle");
+
+  if (img.src.includes("lautsprecher.png")) {
+    img.src = "img/icons/lautsprecher_aus.png";
+    img.alt = "sound off";
+    audioOff();
+  } else {
+    img.src = "img/icons/lautsprecher.png";
+    img.alt = "sound on";
+    audioOn();
+  }
+}
+
+function audioOff() {
+  world.background_music.pause();
+  // world.got_coin_music.pause();
+  // world.got_poison_music.pause();
+  // character.hit_by_jelly.pause();
+  // character.hit_by_fish.pause();
+}
+
+function audioOn() {
+  world.background_music.play();
+  // world.got_coin_music.play();
+  // world.got_poison_music.play();
+  // character.hit_by_jelly.play();
+  // character.hit_by_fish.play();
+}
