875bd7794849074d1a3f09423c385cae24b2d6ce
Amalia Konstantinidou <130513280+Ama-kon@users.noreply.github.com>
2024-05-23 21:09:25 +0200

    jellys now can die from bubble attack

--- models/enemy-jellyfish-lila.class.js
+++ models/enemy-jellyfish-lila.class.js
@@ -8,19 +8,34 @@ class enemyJellyfishLila extends movableObject {
     "img/2.Enemy/2 Jelly fish/Regular damage/Lila 4.png",
   ];
 
-  constructor() {
-    super().loadIMG("img/2.Enemy/2 Jelly fish/Regular damage/Yellow 1.png");
-    this.damageType = "electric";
-    this.x = 1000;
-    this.loadImages(this.images_move);
-    this.speed = 0.3 + Math.random() * 0.5;
-    this.checkSwimDirectionJelly(190);
+  images_die = [
+    "img/2.Enemy/2 Jelly fish/Dead/Lila/L1.png",
+    "img/2.Enemy/2 Jelly fish/Dead/Lila/L2.png",
+    "img/2.Enemy/2 Jelly fish/Dead/Lila/L3.png",
+    "img/2.Enemy/2 Jelly fish/Dead/Lila/L4.png",
+  ];
 
+  constructor() {
+    super();
+    this.loadImages(this.images_move);
+    this.damageType = "electric";
+    this.x = 900 + Math.random() * 500;
+    console.log("lila jelly is ", this.x);
+    this.y = 30 + Math.random() * 300;
+    this.loadImages(this.images_move);
+    this.loadImages(this.images_die);
+    this.speed = 0.2 + Math.random() * 0.2;
+    this.checkSwimDirectionJelly(120, 320);
     this.animate();
   }
   animate() {
     setInterval(() => {
       this.playAnimation(this.images_move);
+      if (this.enemyDying) {
+        this.playAnimation(this.images_die);
+        this.x += 15;
+        this.y -= 25;
+      }
     }, 150);
   }
 }
--- models/enemy-jellyfish-yellow.class.js
+++ models/enemy-jellyfish-yellow.class.js
@@ -1,6 +1,8 @@
 class enemyJellyfishYellow extends movableObject {
   height = 100;
   width = 90;
+  speed = 5;
+
   images_move = [
     "img/2.Enemy/2 Jelly fish/Regular damage/Yellow 1.png",
     "img/2.Enemy/2 Jelly fish/Regular damage/Yellow 2.png",
@@ -8,19 +10,32 @@ class enemyJellyfishYellow extends movableObject {
     "img/2.Enemy/2 Jelly fish/Regular damage/Yellow 4.png",
   ];
 
+  images_die = [
+    "img/2.Enemy/2 Jelly fish/Dead/Yellow/y1.png",
+    "img/2.Enemy/2 Jelly fish/Dead/Yellow/y2.png",
+    "img/2.Enemy/2 Jelly fish/Dead/Yellow/y3.png",
+    "img/2.Enemy/2 Jelly fish/Dead/Yellow/y4.png",
+  ];
+
   constructor() {
     super().loadIMG("img/2.Enemy/2 Jelly fish/Regular damage/Yellow 1.png");
     this.damageType = "electric";
-    this.x = 250 + Math.random() * 400;
+    this.x = 500 + Math.random() * 400;
+    this.y = 50 + Math.random() * 300;
     this.loadImages(this.images_move);
+    this.loadImages(this.images_die);
     this.speed = 0.2 + Math.random() * 0.2;
-    this.checkSwimDirectionJelly(150);
-
+    this.checkSwimDirectionJelly(120, 320);
     this.animate();
   }
   animate() {
     setInterval(() => {
       this.playAnimation(this.images_move);
+      if (this.enemyDying) {
+        this.playAnimation(this.images_die);
+        this.x += 15;
+        this.y -= 25;
+      }
     }, 150);
   }
 }
