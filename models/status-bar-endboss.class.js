15191abdf1ad43f12ebf43330c638b58b3337af0
Amalia Konstantinidou <130513280+Ama-kon@users.noreply.github.com>
2024-05-29 11:41:25 +0200

    status bar for endboss

---
+++ models/status-bar-endboss.class.js
@@ -0,0 +1,45 @@
+class StatusBarEndboss extends DrawableObject {
+  otherDirection = true;
+  images_lifes = [
+    "img/4. Marcadores/orange/100_  copia.png",
+    "img/4. Marcadores/orange/80_  copia.png",
+    "img/4. Marcadores/orange/60_  copia.png",
+    "img/4. Marcadores/orange/40_  copia.png",
+    "img/4. Marcadores/orange/20_ copia 2.png",
+    "img/4. Marcadores/orange/0_  copia.png",
+  ];
+
+  percent = 100;
+  constructor() {
+    super();
+    this.loadImages(this.images_lifes);
+    this.x = 3260;
+    this.y = 0;
+    this.width = 200;
+    this.height = 80;
+    this.setPercent(100);
+  }
+
+  setPercent(percent) {
+    this.percent = percent;
+    let imagePath = this.images_lifes[this.findIndexOfStatusImg()];
+    this.img = this.imageCache[imagePath];
+    console.log(this.percent);
+  }
+
+  findIndexOfStatusImg() {
+    if (this.percent == 100) {
+      return 0;
+    } else if (this.percent >= 80) {
+      return 1;
+    } else if (this.percent >= 60) {
+      return 2;
+    } else if (this.percent >= 40) {
+      return 3;
+    } else if (this.percent >= 20) {
+      return 4;
+    } else {
+      return 5;
+    }
+  }
+}
