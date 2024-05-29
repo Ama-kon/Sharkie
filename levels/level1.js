0310bb32e526f63a9201a30015caa13539f0df20
Amalia Konstantinidou <130513280+Ama-kon@users.noreply.github.com>
2024-05-29 20:40:41 +0200

    outsourced endboss from level to world

--- levels/level1.js
+++ levels/level1.js
@@ -8,7 +8,7 @@ const level1 = new Level(
     new enemyLilaFish(),
     new enemyJellyfishLila(),
     new enemyJellyfishYellow(),
-    new Endboss(),
+    // new Endboss(),
   ],
   [new clouds()],
   [
@@ -109,6 +109,34 @@ const level1 = new Level(
     new BackgroundObjects(
       "img/3. Background/Layers/2. Floor/D1.png",
       720 * 4,
+      0
+    ),
+
+    ////////////////////////////////////////////
+
+    new BackgroundObjects(
+      "img/3. Background/Layers/5. Water/D2.png",
+      720 * 5,
+      0
+    ),
+    new BackgroundObjects(
+      "img/3. Background/Layers/1. Light/2.png",
+      720 * 5,
+      0
+    ),
+    new BackgroundObjects(
+      "img/3. Background/Layers/4.Fondo 2/D2.png",
+      720 * 5,
+      0
+    ),
+    new BackgroundObjects(
+      "img/3. Background/Layers/3.Fondo 1/D2.png",
+      720 * 5,
+      0
+    ),
+    new BackgroundObjects(
+      "img/3. Background/Layers/2. Floor/D2.png",
+      720 * 5,
       0
     ),
   ],
