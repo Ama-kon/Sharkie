4862a88315531054efb4cadaf6cce8b00fcfd74f
Amalia Konstantinidou <130513280+Ama-kon@users.noreply.github.com>
2024-06-13 23:52:55 +0200

    changed: sound function when muted & win and lose game speech

--- js/audio.js
+++ js/audio.js
@@ -27,12 +27,22 @@ got_poison_music = new Audio("audio/got_poison.wav");
 got_poison_music.volume = 0.1;
 
 you_win = new Audio("audio/you_win.mp3");
-you_win.volume = 0.2;
+you_win.volume = 0.05;
 you_win.loop = true;
 
 game_over = new Audio("audio/game_over.wav");
+game_over.volume = 0.03;
+game_over.loop = true;
+
+congratulations_speech = new Audio("audio/Congratulations You .mp3");
+congratulations_speech.playbackRate = 1.5;
+
+lost_game_speech = new Audio("audio/Too Bad You gave it .mp3");
+lost_game_speech.playbackRate = 1.4;
 
 isMuted = false;
+congratulations_speech_told = false;
+lost_game_speech_told = false;
 
 function toggleSound() {
   let img = document.getElementById("sound_toggle");
@@ -51,17 +61,43 @@ function toggleSound() {
 }
 
 function checkSound() {
-  if (isMuted) {
-    audioOff();
-  } else {
-    audioOn();
+  background_music.play();
+  setInterval(() => {
+    if (isMuted) {
+      background_music.pause();
+      endboss_sound.pause();
+      striked_fish.pause();
+      endboss_hurt_sound.pause();
+      hit_by_fish.pause();
+      hit_by_jelly.pause();
+      striked_fish.pause();
+      striked_jelly.pause();
+      got_coin_music.pause();
+      got_poison_music.pause();
+      you_win.pause();
+      game_over.pause();
+      congratulations_speech.pause();
+      lost_game_speech.pause();
+    }
+  }, 1000 / 60);
+}
+
+function playWinningSpeech() {
+  if (!isMuted && !congratulations_speech_told) {
+    congratulations_speech.play();
+
+    setTimeout(() => {
+      congratulations_speech_told = true;
+    }, 23350);
   }
 }
 
-function audioOff() {
-  background_music.pause();
-}
+function playLostGameSpeech() {
+  if (!isMuted && !lost_game_speech_told) {
+    lost_game_speech.play();
 
-function audioOn() {
-  background_music.play();
+    setTimeout(() => {
+      lost_game_speech_told = true;
+    }, 23350);
+  }
 }
