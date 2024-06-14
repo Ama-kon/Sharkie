a4a16a5c649be34627472334cd31734e203aa323
Amalia Konstantinidou <130513280+Ama-kon@users.noreply.github.com>
2024-06-14 23:14:24 +0200

    blur effect for background when game is over

--- js/end-of-game.js
+++ js/end-of-game.js
@@ -1,5 +1,8 @@
 function endScreen(id) {
   let container = document.getElementById(id);
+  let blur = document.getElementById("blur-container");
   container.classList.remove("d-none");
+  blur.classList.remove("d-none");
   container.style.display = "flex";
+  blur.style.display = "flex";
 }
