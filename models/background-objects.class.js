c2a9f856a6435f676be0de334996209b12fb2aba
Amalia Konstantinidou <130513280+Ama-kon@users.noreply.github.com>
2024-06-18 23:40:01 +0200

    documentation only

--- models/background-objects.class.js
+++ models/background-objects.class.js
@@ -1,3 +1,12 @@
+/**
+ * Represents a background object that can be moved on the screen.
+ *
+ * @class BackgroundObjects
+ * @extends movableObject
+ * @param {string} path - The file path to the image for the background object.
+ * @param {number} x - The initial x-coordinate of the background object.
+ * @param {number} y - The initial y-coordinate of the background object.
+ */
 class BackgroundObjects extends movableObject {
   height = 480;
   width = 720;
