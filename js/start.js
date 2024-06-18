716a5ff976f430c5058fcd024f213b2bde903afb
Amalia Konstantinidou <130513280+Ama-kon@users.noreply.github.com>
2024-06-18 23:37:10 +0200

    doumentation only

--- js/start.js
+++ js/start.js
@@ -1,6 +1,11 @@
 window.addEventListener("resize", resizeCanvas);
 document.addEventListener("DOMContentLoaded", toggleFullscreen);
 
+/**
+ * Resizes the canvas element to fit the current window width.
+ * The canvas width is set to 630 pixels if the window width is less than 600 pixels,
+ * 720 pixels if the window width is less than 760 pixels, and 1000 pixels otherwise.
+ */
 function resizeCanvas() {
   let canvas = document.getElementById("canvas");
 
@@ -13,11 +18,37 @@ function resizeCanvas() {
   }
 }
 
+/**
+ * Initializes the game world and canvas.
+ * This function is called to set up the initial state of the game.
+ * It retrieves the canvas element from the DOM and creates a new World instance,
+ * passing the canvas and keyboard objects as parameters.
+ */
+function init() {
+  canvas = document.getElementById("canvas");
+  world = new World(canvas, keyboard);
+}
+
+/**
+ * Initializes the game world, starts the first level, and hides the start screen.
+ * This function is called to begin the game after the user has clicked the start button.
+ * It calls the `initLevel1()` function to set up the initial game state, the `init()` function
+ * to initialize the canvas and game world, and the `checkSound()` function to ensure sound
+ * is enabled. Finally, it hides the start screen element.
+ */
 function startGame() {
+  initLevel1();
+  init();
+  checkSound();
   let startScreen = document.getElementById("start_game");
   startScreen.classList.add("d-none");
 }
 
+/**
+ * Toggles the visibility of the instructions sheet element on the page.
+ * If the instructions sheet is currently hidden, it will be shown with a fade-in animation.
+ * If the instructions sheet is currently visible, it will be hidden with a fade-out animation.
+ */
 function toggleInstructions() {
   let instructions = document.getElementById("instructions_sheet");
   if (instructions.classList.contains("d-none")) {
@@ -33,6 +64,12 @@ function toggleInstructions() {
   }
 }
 
+/**
+ * Toggles the fullscreen mode of the canvas element.
+ * This function adds event listeners to the document to detect when the fullscreen mode is exited,
+ * and updates the state of the fullscreen toggle button accordingly. It also handles the logic
+ * to request and exit fullscreen mode for the canvas element.
+ */
 function toggleFullscreen() {
   const canvas = document.getElementById("canvas");
   const fullscreenToggle = document.getElementById("fullscreenToggle");
@@ -43,12 +80,7 @@ function toggleFullscreen() {
   document.addEventListener("MSFullscreenChange", exitHandler);
 
   function exitHandler() {
-    if (
-      !document.fullscreenElement &&
-      !document.webkitFullscreenElement &&
-      !document.mozFullScreenElement &&
-      !document.msFullscreenElement
-    ) {
+    if (!document.fullscreenElement) {
       fullscreenToggle.checked = false;
     }
   }
